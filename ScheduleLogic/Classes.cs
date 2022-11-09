6b5db63e90f803f1a36654a3c4631b8b6dac1fcd
0xKolyanus <mukola.1zt.@gmail.com>
2022-11-09 22:11:56 +0200

    updated

---
+++ ScheduleLogic/Classes.cs
@@ -0,0 +1,24 @@
+﻿namespace ScheduleLogic
+{
+    public class Classes
+    {
+        public Teacher Teacher { get; set; }
+        public Lesson Lesson { get; set; }
+        public Audience Audience { get; set; }
+        
+        public Classes()
+        {
+            Teacher = new Teacher();
+            Lesson = new Lesson();
+            Audience = new Audience();
+        }
+        public Classes(Teacher teacher, Lesson lesson, Audience audience)
+        {
+            Teacher = teacher;
+            Lesson = lesson;
+            Audience = audience;
+        }        
+
+
+    }
+}
--- ScheduleLogic/Core.cs
+++ ScheduleLogic/Core.cs
@@ -6,9 +6,17 @@
         public HashSet<Group> groups;
         public HashSet<Audience> audiences;
         public HashSet<Lesson> lessons;
+        public HashSet<Teacher> freeTeachers;
+        public HashSet<Audience> freeAudiences;
+
+        public int countOfWorkingDay = 5;
+        public int countOfMaxClassesInDay = 6;
 
         public Dictionary<Group, HashSet<Lesson>> listLessonForGroup;
 
+        public Dictionary<Group, List<Classes>> shedule;
+
+        #region ctor
         public Core()
         {
             teachers = new HashSet<Teacher>();
@@ -16,6 +24,7 @@
             audiences = new HashSet<Audience>();
             lessons = new HashSet<Lesson>();
             listLessonForGroup = new Dictionary<Group, HashSet<Lesson>>();
+            shedule = new List<Dictionary<Group, Classes>>();
         }
 
         public Core(HashSet<Teacher> teachers,
@@ -28,6 +37,7 @@
             this.audiences = audiences;
             this.lessons = lessons;
             listLessonForGroup = new Dictionary<Group, HashSet<Lesson>>();
+            shedule = new List<Dictionary<Group, Classes>>();
         }
 
         public Core(HashSet<Teacher> teachers,
@@ -35,12 +45,57 @@
                     HashSet<Audience> audiences,
                     HashSet<Lesson> lessons,
                     Dictionary<Group, HashSet<Lesson>> listLessonForGroup)
-            : this(teachers, groups, audiences, lessons) 
+            : this(teachers, groups, audiences, lessons)
             => this.listLessonForGroup = listLessonForGroup;
+        #endregion
 
-        public void GenerateShedule()
+        private Teacher? GetFreeTeacher(Lesson requiredLesson)
         {
+            foreach (var teacher1 in freeTeachers)
+            {
+                foreach (var lesson1 in teacher1.lessonsToTeach)
+                {
+                    if (lesson1 == requiredLesson)
+                    {
+                        return teacher1;
+                    }
+                }
+            }
+            return null;
+        }
 
+        private Classes? GenerateClasses()
+        {
+            var currentLesson = item.Value.FirstOrDefault();
+            if (currentLesson == null)
+            {
+                return null;
+            }
+            var currentTeacher = GetFreeTeacher(currentLesson);
+            if (currentTeacher == null)
+            {
+                throw new ArgumentException("Не хватає вільних вчителів");
+            }
+
+            return new Classes(currentTeacher, currentLesson, freeAudiences.FirstOrDefault()); 
+        }
+
+        public Dictionary<Group, List<Classes>> GenerateShedule()
+        {
+            shedule = new Dictionary<Group, List<Classes>>();
+                        
+            foreach (var item in listLessonForGroup)
+            {
+                // to do: add logic \ cycle
+                var currentClasses = GenerateClasses();
+                if (currentClasses == null)
+                {
+                    continue;
+                }
+                shedule.Add(item.Key, currentClasses);
+            }
+
+            return shedule;
         }
     }
 }
